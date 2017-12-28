011113a2c9e84ec0ef6a4fddfd9e613b11872ed3
Wony <dnjs3013@naver.com>
2017-12-28 15:22:19 +0900

    주석 추가

--- src/main/java/kr/or/nextit/board/controller/BoardController.java
+++ src/main/java/kr/or/nextit/board/controller/BoardController.java
@@ -1,15 +1,10 @@
 package kr.or.nextit.board.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kr.or.nextit.board.model.Board;
 import kr.or.nextit.board.service.BoardService;
 
 @Controller
